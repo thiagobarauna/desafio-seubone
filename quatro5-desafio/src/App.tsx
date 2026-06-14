d55d50c7e1a6d605b0e063ed0803c64f066dfdfc
thiagobarauna <thiago2017barauna@gmail.com>
2026-06-14 11:08:37 -0300

    initial commit

---
+++ quatro5-desafio/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Desafio Técnico: Quatro5</h1>
+      <p>Bainnnnn jotapaulo</p>
+
+      <div className="instructions">
+        <h2>Sua Missão</h2>
+        <p>rebolar pros cria gostosinho</p>
+        <ul>
+          <li>penis</li>
+          <li>chaves</li>
+          <li>aiaiaiiaiaia</li>
+          <li>saudades da mnha ex volta vida.</li>
+        </ul>
+        <p><strong>Boa sorte.</strong></p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
